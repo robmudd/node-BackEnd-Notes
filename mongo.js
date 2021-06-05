6d116fe472acd5dd3658e49e8bbd86bfe312b4ea
rob <robmudd@gmail.com>
2021-06-05 13:27:43 +0200

    Mongoose functionality works for testing

--- mongo.js
+++ mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://robmudd:${password}@cluster0.zzzoh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const url = `mongodb+srv://robmudd:${password}@cluster0.zzzoh.mongodb.net/notes-app?retryWrites=true&w=majority`
 
 mongoose.connect(url, {
     useNewUrlParser: true,
@@ -20,4 +20,32 @@ const noteSchema = new mongoose.Schema({
     content: String,
     date: Date,
     important: Boolean
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+/*
+const notes = [new Note({
+    content: 'GitHub can be confusing',
+    date: new Date(),
+    important: true,
+}), new Note({
+    content: 'I need to learn databases',
+    date: new Date(),
+    important: true,
+})]
+
+for (let note of notes) {
+    note.save().then(result => {
+        console.log('note saved!')
+        console.log(result)
+        mongoose.connection.close()
+    })
+}*/
+
+Note.find({}).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
+    mongoose.connection.close()
 })
